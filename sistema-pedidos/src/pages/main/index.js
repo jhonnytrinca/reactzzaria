405436b6649cfa10bd7953cef6f3f172dfec2957
Jhonny Trinca <jhonny.trinca@hotmail.com>
2021-11-11 17:24:41 -0300

    CSS aplicado no grid de pizzas

--- sistema-pedidos/src/pages/main/index.js
+++ sistema-pedidos/src/pages/main/index.js
@@ -10,6 +10,7 @@ import {
   Grid,
   withStyles,
   Paper,
+  Divider as MaterialDivider,
 } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
 import { ReactComponent as MainLogo } from "../../images/logo-react-zzaria.svg";
@@ -53,32 +54,103 @@ const Main = () => {
 
       <Content>
         <Grid container direction="column" alignItems="center">
-          <Typography variant="h3">O que vai ser hoje, {userName}?</Typography>
-          <Typography variant="h4">Escolha o tamanho das pizzas:</Typography>
+          <Title variant="h3">O que vai ser hoje, {userName}?</Title>
+          <Title variant="h4">Escolha o tamanho das pizzas:</Title>
         </Grid>
-        <Grid container spacing={16}>
+        <PizzasGrid>
           {PizzaSizes.map((pizza) => (
-            <Grid item key={pizza.id} xs={4}>
-              <Paper style={{ padding: 20 }}>
-                <div>{pizza.size}cm</div>
-                <Typography>{pizza.name}</Typography>
+            <Grid item key={pizza.id} xs>
+              <PaperPizza>
+                <Pizza>
+                  <PizzaText>{pizza.size}cm </PizzaText>
+                </Pizza>
+                <Divider />
+                <Typography variant="h5">{pizza.name}</Typography>
                 <Typography>
-                  {pizza.slices} fatias, {pizza.flavours} sabores
+                  {pizza.slices} fatias, {pizza.flavours}{" "}
+                  {singularOrPlural(pizza.flavours, "sabor", "sabores")}{" "}
                 </Typography>
-              </Paper>
+              </PaperPizza>
             </Grid>
           ))}
-        </Grid>
+        </PizzasGrid>
       </Content>
     </>
   );
 };
+
+function singularOrPlural(amount, singular, plural) {
+  return amount === 1 ? singular : plural;
+}
 
 const PizzaSizes = [
   { id: 0, name: "Pequena", size: 28, slices: 2, flavours: 1 },
   { id: 1, name: "Média", size: 30, slices: 6, flavours: 2 },
   { id: 2, name: "Grande", size: 32, slices: 8, flavours: 3 },
 ];
+
+const Title = styled(Typography).attrs({
+  gutterBottom: true,
+  align: "center",
+})``;
+
+const Divider = styled(MaterialDivider)`
+  margin: 20px 0;
+  width: 100%;
+`;
+
+const PaperPizza = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+  min-width: 250px;
+`;
+
+const PizzaText = styled(Typography).attrs({ variant: "h5" })`
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+  background: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 1;
+`;
+
+const Pizza = styled.div`
+  height: 200px;
+  width: 200px;
+  border: 1px solid silver;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  &::before,
+  &::after {
+    content: "";
+    background: silver;
+    position: absolute;
+    transform: rotate(45deg);
+  }
+
+  &::before {
+    height: 1px;
+    width: 160px;
+  }
+
+  &::after {
+    height: 160px;
+    width: 1px;
+  }
+`;
+
+const PizzasGrid = styled(Grid).attrs({ container: true, spacing: 16 })`
+  padding: 20px;
+`;
 
 const Toolbar = styled(MaterialToolbar)`
   width: 100%;
