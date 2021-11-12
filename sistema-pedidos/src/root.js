e9be568624acdfc1db9307c2c460f248c03a21fb
Jhonny Trinca <jhonny.trinca@hotmail.com>
2021-11-12 12:28:43 -0300

    atualização dependências

--- sistema-pedidos/src/root.js
+++ sistema-pedidos/src/root.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import {
-  CssBaseline,
-  createMuiTheme,
-  MuiThemeProvider,
-} from "@material-ui/core";
+import { CssBaseline, createTheme, MuiThemeProvider } from "@material-ui/core";
 import App from "./app";
 import AuthProvider from "./contexts/auth";
 
-const theme = createMuiTheme({
+const theme = createTheme({
   typography: {
     useNextVariants: true,
   },
